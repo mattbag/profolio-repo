4e1cae018aae4f95061fab4c000179d779e5bf95
matt <matt@atlasagency.com.au>
2017-09-13 10:15:55 +1000

    social loop

--- src/components/social/social.js
+++ src/components/social/social.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './social.scss'
 class Social extends React.Component {
     render() {
-        const links = [{ name: 'github', link: 'https://mattbag.github.io/' },
-        { name: 'linkedin', link: 'https://www.linkedin.com/in/matteobagni88/' },
-        { name: 'twitter', link: 'https://mobile.twitter.com/mattbag00' },
-        { name: 'codepen', link: 'https://www.linkedin.com/in/matteobagni88/' },]
+        const links = [
+            { name: 'github', link: 'https://mattbag.github.io/' },
+            { name: 'linkedin', link: 'https://www.linkedin.com/in/matteobagni88/' },
+            { name: 'twitter', link: 'https://mobile.twitter.com/mattbag00' },
+            { name: 'codepen', link: 'https://codepen.io/mattbag/' },]
 
         const aStyle = {
             fontFamily: 'Lobster',
@@ -16,7 +17,7 @@ class Social extends React.Component {
 
         return (
             <ul className="social" style={{ margin: 0 }}>
-                {links.map(link=>{
+                {links.map(link => {
                     return <li><a href={link.link} target="_blank" style={aStyle}>{link.name}</a></li>
                 })}
             </ul>
